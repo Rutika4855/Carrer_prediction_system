b12e33bebe658e335478463ff823ab12d2858262
Rutika4855 <dhobalerutika25@gmail.com>
2026-09-01 10:59:43 +0530

    Fix email port environment variable

--- career_prediction_system/settings.py
+++ career_prediction_system/settings.py
@@ -110,7 +110,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/6.0/topics/email/
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' if os.environ.get('EMAIL_HOST') else 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.example.com')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT') or 587)
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True').lower() == 'true'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
